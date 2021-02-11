3a580ead7f2d8829440aa1f4f316cf898b8af6b1
kosukesaigusa <saigusa758cloudy@gmail.com>
2021-02-11 09:46:07 +0900

    feat: added common appbar widget

---
+++ lib/common/appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqflite_practice/common/constants.dart';
+
+PreferredSize customAppBar = PreferredSize(
+  preferredSize: Size.fromHeight(0.0),
+  child: AppBar(
+    backgroundColor: greyColor,
+    shadowColor: Colors.transparent,
+  ),
+);
